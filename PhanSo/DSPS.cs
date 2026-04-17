e1e82f7f844ed26b52d2b48655392d2de4b9da7f
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-04-17 21:01:08 +0700

    Phan_So

---
+++ PhanSo/DSPS.cs
@@ -0,0 +1,86 @@
+﻿using Bai2_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_2
+{
+    public class DSPS
+    {
+        private PhanSo[] _dsPS;
+        private int _size;
+
+        public PhanSo[] DsPS { get => _dsPS; set => _dsPS = value; }
+        public int Size { get => _size; set => _size = value; }
+
+        public DSPS()
+        {
+            _size = 0;
+            _dsPS = new PhanSo[_size];
+        }
+        public void NhapPS()
+        {
+            Console.WriteLine("Nhap so luong phan tu ");
+            _size = int.Parse(Console.ReadLine());
+            _dsPS = new PhanSo[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                Console.WriteLine("Nhap phan so thu {0}:", i + 1);
+                _dsPS[i] = new PhanSo();
+                _dsPS[i].Nhap();
+            }
+        }
+        public void XuatPS()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                Console.WriteLine("Phan so thu {0}:", i + 1);
+                _dsPS[i].Xuat();
+            }
+        }
+        public int sosanh(PhanSo a, PhanSo b)
+        {
+            int tua = a.TuSo * b.MauSo;
+            int tub = b.TuSo * a.MauSo;
+            if (tua > tub)
+                return 1;
+            else if (tua < tub)
+                return -1;
+            else
+                return 0;
+
+        }
+        public PhanSo Max()
+        {
+            PhanSo max = _dsPS[0];
+            for (int i = 1; i < _size; i++)
+            {
+                if (sosanh(_dsPS[i], max) == 1)
+                {
+                    max = _dsPS[i];
+                }
+            }
+            return max;
+        }
+        public void TangDan()
+        {
+            for(int i = 0; i < _size-1; i++)
+            {
+                for(int j = i + 1; j < _size; j++)
+                {
+                    if(sosanh(_dsPS[i], _dsPS[j]) == 1)
+                    {
+                       PhanSo temp = _dsPS[i];
+                        _dsPS[i] = _dsPS[j];
+                        _dsPS[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+
+}
+}
---
+++ PhanSo/Program.cs
@@ -0,0 +1,100 @@
+﻿using Bai2_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_1
+{
+    public class PhanSo
+    {
+        private int tuSo;
+        private int mauSo;
+
+        public int TuSo { get => tuSo; set => tuSo = value; }
+        public int MauSo { get => mauSo; set => mauSo = value; }
+        public PhanSo(int tuSo = 0, int mauSo = 1)
+        {
+            this.tuSo = tuSo;
+            this.mauSo = mauSo;
+        }
+        public PhanSo(PhanSo ps, int tuSo)
+        {
+            tuSo = ps.tuSo;
+            mauSo = ps.MauSo;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap tu so: ");
+            tuSo = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau so: ");
+            mauSo = int.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Phan so: {0}/{1}", tuSo, mauSo);
+        }
+        public void ToiGian()
+        {
+            int a = tuSo;
+            int b = mauSo;
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+
+                }
+            }
+            tuSo /= a;
+            mauSo /= a;
+            Console.WriteLine("Phan so toi gian: {0}/{1}", tuSo, mauSo);
+        }
+        public PhanSo Cong(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuSo = tuSo * ps.mauSo + mauSo * ps.tuSo;
+            kq.mauSo = mauSo * ps.mauSo;
+            return kq;
+        }
+        public PhanSo Tru(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuSo = tuSo * ps.mauSo - mauSo * ps.tuSo;
+            kq.mauSo = mauSo * ps.mauSo;
+            return kq;
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //PhanSo ps1 = new PhanSo();
+                //ps1.Nhap();
+                //ps1.Xuat();
+                //ps1.ToiGian();
+                //PhanSo ps2 = new PhanSo();
+                //ps2.Nhap();
+                //ps2.Xuat();
+                //ps2.ToiGian();
+                //PhanSo kqCong = ps1.Cong(ps2);
+                //Console.WriteLine("Tong 2 phan so: {0}/{1}", kqCong.TuSo, kqCong.MauSo);
+                //PhanSo kqTru = ps1.Tru(ps2);
+                //Console.WriteLine("Hieu 2 phan so: {0}/{1}", kqTru.TuSo, kqTru.MauSo);
+                DSPS  ds = new DSPS();
+                ds.NhapPS();
+                ds.XuatPS();
+                PhanSo lonnhat =  ds.Max();  
+                Console.WriteLine("Phan so lon nhat: ");
+                lonnhat.Xuat();
+                Console.WriteLine("Tang dan: ");
+                ds.TangDan();
+                ds.XuatPS();
+            }
+        }
+    }
+}
