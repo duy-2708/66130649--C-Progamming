deffefebff970d3b3298a2465d3bcb683b54e1c5
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-04-17 21:00:57 +0700

    Bai_tap_Them

---
+++ Baitap_1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Baitap_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
